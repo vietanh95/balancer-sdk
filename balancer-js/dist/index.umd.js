582c7982def2d3846641fe527a247d55783e07ed
johngrantuk <johngrantuk@googlemail.com>
2021-11-24 15:36:42 +0000

    Package for test.

---
+++ balancer-js/dist/index.umd.js
@@ -0,0 +1,21 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+})((function () { 'use strict';
+
+	Object.defineProperty(exports, "__esModule", { value: true });
+	const tslib_1 = require("tslib");
+	(0, tslib_1.__exportStar)(require("./pool-stable"), exports);
+	(0, tslib_1.__exportStar)(require("./pool-weighted"), exports);
+	(0, tslib_1.__exportStar)(require("./pool-utils"), exports);
+	(0, tslib_1.__exportStar)(require("./utils"), exports);
+	(0, tslib_1.__exportStar)(require("./types"), exports);
+	(0, tslib_1.__exportStar)(require("./swapsService/index"), exports);
+	(0, tslib_1.__exportStar)(require("./swapsService/types"), exports);
+	(0, tslib_1.__exportStar)(require("./swapsService/helpers"), exports);
+	(0, tslib_1.__exportStar)(require("./constants/network"), exports);
+	(0, tslib_1.__exportStar)(require("./constants/subgraph"), exports);
+	(0, tslib_1.__exportStar)(require("./sdk"), exports);
+
+}));
+//# sourceMappingURL=index.umd.js.map
